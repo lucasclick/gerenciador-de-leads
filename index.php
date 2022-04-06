d643e4432dc37f84215db0d5e9aac4cb1467b142
Lucas <102034980+lucasclick@users.noreply.github.com>
2022-04-06 13:48:40 -0300

    Iniciação da iniciação

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php 
-require_once "./includes/header.php" ;
+require_once "./includes/head.php" ;
 ?>
 
     <body>  
+        <header>
             
+        </header>
+        <main>
+            <div id="card-01">
+                <div id="card-title">
+                    <h2> New</h2>
+                    
+                </div>
+            </div>
+            <div id="card-02"></div>
+            <div id="card-03"></div>
+            <div id="card-04"></div>
+            <div id="card-05"></div>
+
+        </main>
 
 
-    </body>
+
     
 
 <?php 
